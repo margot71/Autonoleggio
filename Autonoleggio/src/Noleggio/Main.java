e514f58223872c5328ac59420dd4d53f0dd1da97
Agata <142403159+agw123@users.noreply.github.com>
2024-05-03 12:29:56 +0200

    Update Main.java
    test commit

--- Autonoleggio/src/Noleggio/Main.java
+++ Autonoleggio/src/Noleggio/Main.java
@@ -9,10 +9,8 @@ public class Main {
 		noleggio.aggiungiAuto(new Auto(001, "Fiat Punto", 40.99, false, null));
 		noleggio.aggiungiAuto(new Auto(002, "Ford Fiesta", 45.29, false, null));
 		noleggio.aggiungiAuto(new Auto(003, "AlfaRomeo Giulietta", 70.99, false, null));
-		 
 		noleggio.ricercaAuto(52);
 		noleggio.selezioneAuto(1);
-		
 		noleggio.aggiungiBatMobile( new BatMobile(007, "Batmobile num1"));
 		noleggio.aggiungiBatMobile( new BatMobile(1, "Batmobile num2"));
 		noleggio.aggiungiBatMobile( new BatMobile(2, "Batmoto"));
