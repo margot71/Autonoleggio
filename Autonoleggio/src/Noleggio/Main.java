1577bd1e23c89afa9600fa876f340d8bae6c014a
margot71 <167749953+margot71@users.noreply.github.com>
2024-05-04 15:33:18 +0200

    Update Main con impostazione login

--- Autonoleggio/src/Noleggio/Main.java
+++ Autonoleggio/src/Noleggio/Main.java
@@ -3,7 +3,21 @@ package Noleggio;
 public class Main {
  
 	public static void main(String[] args) {
-
+		String user;
+		String psw;
+		Scanner scan = new Scanner(System.in);
+		//Login utente
+		System.out.println("Inserisci user: ");
+		user = scan.nextLine();
+		System.out.println("Inserisci password: ");
+		psw = scan.nextLine();
+		Login login = new Login();
+		login.inserisciUserPsw(user, psw);
+		
+		//login.inserisciUserPsw restituisce null se utente inesistente o valore numerico per tipo utente
+		//Verifica su tipologia di utente con if
+		//A seconda dell'utente, presentare il menu appropriato con switch
+		
 		Noleggio noleggio = new Noleggio();
 		//nel parametro bisogna aggiungere new, altrimenti sovrascrive ogni volta
 		noleggio.aggiungiAuto(new Auto(001, "Fiat Punto", 40.99, false, null));
